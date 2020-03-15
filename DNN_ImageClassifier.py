fe6b5a260ca5daec2bdd4a733bc015029ba11084
Yashwardhann Kumar <yash20008@gmail.com>
2020-03-15 23:49:53 +0530

    Added training data and accuracy calculation

--- DNN_ImageClassifier.py
+++ DNN_ImageClassifier.py
@@ -1,19 +1,31 @@
+from math import floor
 import h5py
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy.special import expit
 
+# Helper functions
 def load_dataset(): 
+  # Load the training dataset
   train_set = h5py.File('train_catvnoncat.h5', 'r')
   train_set_X = np.array(train_set['train_set_x'])
   train_set_y = np.array(train_set['train_set_y'])
-  train_classes = np.array(train_set['list_classes'])
   # Prepare the dataset 
   train_set_y = train_set_y.reshape(1, train_set_y.shape[0])
-  return train_set_X, train_set_y, train_classes 
+  # Load the testing/dev dataset
+  test_set = h5py.File('test_catvnoncat.h5', 'r')
+  test_set_X = np.array(test_set['test_set_x'])
+  test_set_y = np.array(test_set['test_set_y'])
+  # Prepare the dataset 
+  test_set_y = test_set_y.reshape(1, test_set_y.shape[0])
+  return train_set_X, train_set_y, test_set_X, test_set_y
+
+# Returns the number with 'places' number of decimal places
+def setPrecision(num, places): 
+  return floor(num * 10**places)/10**places
 
 # Load the dataset 
-X, y, classes = load_dataset()
+X, y, X_test, y_test = load_dataset()
 # Get the dataset features 
 m = y.shape[1] # Number of training samples 
 n = X.shape[1] * X.shape[2] * X.shape[3] # Number of features
@@ -23,7 +35,7 @@ X_flatten = X.reshape(m, n).T
 X_flatten = X_flatten/255
 
 # Describe the data 
-print('\n------------ DATASET INFO ---------------\n')
+print('\n------------ DATASET INFO (Training) ---------------\n')
 print('Shape of Input (pre-flatten):', X.shape)
 print('Shape of Input (post-flatten):', X_flatten.shape)
 print('Shape of Output:', y.shape)
@@ -151,34 +163,42 @@ class Model:
       for j in range(1, len(self.layers)): 
         self.parameters['W'+str(j)] = self.parameters['W'+str(j)] - self.learning_rate*grads['dW'+str(j)]
         self.parameters['b'+str(j)] = self.parameters['b'+str(j)] - self.learning_rate*grads['db'+str(j)]
-      # Save the cost 
-      self.J_hist.append(cost)
+
       # Find the growth rate 
       if len(self.J_hist) > 3: 
         epsilon = 1e-5 # Prevent division by zero
         self.growth_rate = (self.J_hist[-1]-self.J_hist[-2])/(self.J_hist[-2]-self.J_hist[-3]+epsilon)
-        
-        if self.growth_rate < 0.25: 
-          # Exit the loop if growth is slow 
-            break
       # Print the cost for every 50 iterations 
       if i%50 == 0: 
-        print(f'Cost at iteration {i}:', cost)
-    print('Final cost is:', cost)
+        # Save the cost 
+        self.J_hist.append(cost)
+        print(f'Cost at iteration {i}:', setPrecision(cost, 4))
+    print('Final cost is:', setPrecision(cost, 4))
     # Flush the cache
     self.cache.clear()
     
   def predict(self, X, prepare=True): 
-    # Check if the input needs to be prepared
-    if prepare is True: 
-      # Flatten the image matrix 
-      X_flatten = X.reshape(64*64*3, 1)
-      # Standardize the values of X
-      X_flatten = X_flatten/255
-    
-    # Get the prediction 
-    prediction = self.feed_forward(X_flatten)
-    print('Prediction:', np.squeeze(prediction))
+    # Get the prediction and convert it into a float
+    prediction = np.squeeze(self.feed_forward(X))
+    prediction = prediction > 0.5
+    return prediction.astype(int)
+
+  # Calculate the training set accuracy 
+  def getAccuracy(self, X, y, isFlat = True): 
+    if isFlat is False: 
+      m = X.shape[0]  
+      # Flatten the matrix
+      X = X.reshape(64*64*3, m)
+      # Standardize the results 
+      X = X/255
+    else: 
+      m = X.shape[1]
+    # Get the predictions of the inputs
+    predictions = self.predict(X)
+    # Get the percentage of correct predictions 
+    accuracy = np.sum((predictions == y)/m)
+    # Return the accuracy as a percentage
+    return accuracy*100
 
   def plotCost(self): 
     # Plot the cost hist
@@ -187,18 +207,24 @@ class Model:
     plt.ylabel('Cost')
     plt.title('Cost function history')
     plt.savefig('Ok.png')
-   
 
 # Two layer neural network 
 layer_dims = [n, 20, 7, 5, 1]
 # Define a new model 
 model = Model({
   'layers': layer_dims, 
-  'learning_rate': 0.0005,
-  'num_iters': 2000
+  'learning_rate': 0.008,
+  'num_iters': 1000
 })
 
 model.train(X_flatten, y)
-
+# Get the training accuracy
+training_accuracy = model.getAccuracy(X_flatten, y)
+# Print the training accuracy with 2 decimal places
+print('The training accuracy is: ' + str(setPrecision(training_accuracy, 2)) + '%')
+# Get the test accuracy 
+test_accuracy = model.getAccuracy(X_test, y_test, False)
+# Print the test accuracy with 2 decimal places
+print('The test accuracy is: ' + str(setPrecision(test_accuracy, 2)) + '%')
 # Plot the final cost
 model.plotCost()
